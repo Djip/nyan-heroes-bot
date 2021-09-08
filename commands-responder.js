ee613e92219ee5e7393670ba6ccc61531930f656
Djip <jesper@finn-madsen.dk>
2021-09-08 03:06:44 +0200

    Rank card WIP

--- commands-responder.js
+++ commands-responder.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const { Client, Intents } = require('discord.js');
 const fs = require("fs");
 const nodeHtmlToImage = require("node-html-to-image");
+const axios = require('./modules/axios')
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
@@ -17,24 +18,48 @@ client.on('interactionCreate', async interaction => {
     }
 
     if (interaction.commandName === 'test') {
-        await fs.readFile(__dirname + '/rank-card/index.html', async (err, data) => {
+        const api = await axios.api()
+        fs.readFile(__dirname + '/rank-card/index.html', async (err, data) => {
             const backgroundImage = fs.readFileSync('./rank-card/background.png');
             const base64BackgroundImage = new Buffer.from(backgroundImage).toString('base64');
             const backgroundImageUri = 'data:image/png;base64,' + base64BackgroundImage;
 
-            const rankCard = await nodeHtmlToImage({
-                html: data.toString(),
-                content: {
-                    backgroundImage: backgroundImageUri,
-                    avatar: interaction.user.avatarURL(),
+            let rankData;
+            await api.get(`users/rank`, {
+                params: {
                     username: interaction.user.username,
                     discriminator: interaction.user.discriminator
-                },
-                transparent: true
+                }
+            }).then(response => {
+                console.log(response);
+                rankData = response.data
+            }).catch(error => {
+                console.log(error);
+                if (interaction) {
+                    interaction.reply('Error, try again.')
+                }
             })
 
-            if (interaction) {
-                interaction.reply({files: [rankCard]})
+            if (rankData) {
+                const rankCard = await nodeHtmlToImage({
+                    html: data.toString(),
+                    content: {
+                        backgroundImage: backgroundImageUri,
+                        avatar: interaction.user.avatarURL(),
+                        username: interaction.user.username,
+                        discriminator: interaction.user.discriminator,
+                        rank: rankData.rank,
+                        level: rankData.level.level,
+                        currentXp: rankData.xp,
+                        nextLevel: rankData.next_level.level,
+                        nextXp: rankData.next_level.xp_required
+                    },
+                    transparent: true
+                })
+
+                if (interaction) {
+                    interaction.reply({files: [rankCard]})
+                }
             }
         });
     }

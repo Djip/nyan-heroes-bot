189fbafb5366c75038837d1fd6310d731b1923ff
Djip <jesper@finn-madsen.dk>
2021-09-08 03:05:59 +0200

    Finished discord invited checker

--- discord-invite-checker.js
+++ discord-invite-checker.js
@@ -2,9 +2,10 @@ require('dotenv').config();
 
 const { REST }  = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { Client, Intents} = require('discord.js')
+const { Client, Intents} = require('discord.js');
 const fs = require('fs');
 const nodeHtmlToImage = require('node-html-to-image');
+const axios = require('./modules/axios')
 
 const rest = new REST({version: '9'}).setToken(process.env.DISCORD_BOT_TOKEN);
 const client = new Client({intents: [Intents.FLAGS.GUILDS]});
@@ -12,7 +13,22 @@ client.login(process.env.DISCORD_BOT_TOKEN);
 
 (async () => {
     try {
+        const api = await axios.api();
+
         let channel = null;
+
+        await rest.get(Routes.guildInvites(process.env.DISCORD_NYAN_HEROES_GUILT_ID)).then(async response => {
+            for (const invite of response) {
+                if (invite.uses > 0) {
+                    api.post('discord-invites/give-xp', invite).then(response => {
+                        console.log(response)
+                    }).catch(error => {
+                        console.log(error)
+                    })
+                }
+            }
+        })
+        return;
 
         await client.guilds.fetch(process.env.DISCORD_NYAN_HEROES_GUILT_ID).then(async response => {
             await response.channels.fetch('880448290477465642').then(async data => {
@@ -41,8 +57,8 @@ client.login(process.env.DISCORD_BOT_TOKEN);
         if (channel) {
             await rest.get(Routes.guildInvites(process.env.DISCORD_NYAN_HEROES_GUILT_ID)).then(response => {
                 response.forEach(invite => {
+                    console.log(invite);
                     if (invite.uses > 0) {
-                        let xp = invite.uses * 10;
                         channel.send(invite.inviter.username);
                         console.log(invite.inviter.username)
                     }
